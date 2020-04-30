53a372f203f2a254ba2b8b86844534dd358ff979
john-shiner <john.shiner@gmail.com>
2020-04-29 23:09:12 -0500

    basic procedure for loading log data

--- loadLogFile.py
+++ loadLogFile.py
@@ -9,6 +9,8 @@ port = config.REDIS_CFG["port"]
 pwd = config.REDIS_CFG["password"]
 db = config.REDIS_CFG["db"]
 redis = Redis(db=db, host=host, port=port, password=pwd, charset="utf-8", decode_responses=True)
+
+# Populate the logEntry hash structures in a server-side batch pipeline
 pipe = redis.pipeline(transaction=False)
 
 # Import Log files
@@ -18,24 +20,29 @@ count = 0
 
 with open('data/workfile') as f:
 
-
+    # Each row is a logEntry
     for read_data in f:
         count = count+1
         logEntryKey = "logEntry:{}".format(count)
-        print(logEntryKey)       
+        print(logEntryKey)
+
+        # Split each logEntry into separate fields
+        # Each field is a key-value pair
+
         lst = read_data.split("|")
-        # print(lst)
+ 
+
         lst.pop(35)
         lst.pop(0)
         lst.pop(30)
         for i in lst:
-
+           # Replace the first ':' character with a '|' to obtain proper k-v split
            j = i.replace(":", "|", 1)
 
            k, v = j.split("|")
-           # print("k = {}, v = {}".format(k, v))
+
+           # Populate the logEntryKey hash for each logEntry key-value pair
            pipe.hset(logEntryKey, k, v )
         pipe.execute()
-
 
 print("Import of {} records completed.".format(count))
