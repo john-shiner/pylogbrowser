cd934e173627691278a77167823d72f900a83c06
john-shiner <john.shiner@gmail.com>
2020-05-01 13:12:43 -0500

    wip loader

--- loadLogFile.py
+++ loadLogFile.py
@@ -13,9 +13,54 @@ redis = Redis(db=db, host=host, port=port, password=pwd, charset="utf-8", decode
 # Populate the logEntry hash structures in a server-side batch pipeline
 pipe = redis.pipeline(transaction=False)
 
-# Import Log files
-print("Importing ...")
+# # Import Log files
+# print("Importing ...")
 
+indexValueMaps = {}
+
+supportedIndices = ["client_host", "client_id", "client_ip", \
+                    "environment", "organization", "proxy", "proxy_basepath", "proxy_name", \
+                    "proxy_revision", "request_path", "request_uri", "request_verb", \
+                    "response_reason_phrase", "response_status_code", "soap_operation", \
+                    "soap_siteId", "target_basepath", "target_host", "target_ip", "virtual_host"]
+
+class IndexMgr:
+    def __init__(self, fieldName):
+        self.fieldName = fieldName
+        self.valueMap = {}
+        self.valueSet = set()
+    def setadd(self, fieldValue):
+        self.valueSet.add(str(fieldValue))
+    def add(self, logEntryKey, fieldValue):
+        self.valueMap[logEntryKey] = str(fieldValue)
+
+
+# idx:<i>
+# set:values:<i>
+# hash:values:<i>:<valueId>
+
+# for i in set:values:<i>
+# logIds = map(lambda x: x.split(":")[1], le)
+
+for i in supportedIndices:
+    indexValueMaps[i] = IndexMgr(i)
+    keys = redis.keys("logEntry:*")
+    # logEntryIds = map(lambda x: x.split(":")[1], keys)
+    for k in keys:
+        # breakpoint()
+        # logEntryId = k.split(":")[1]
+        mappedValue = redis.hget(k, i)
+        indexValueMaps[i].setadd(mappedValue)
+        indexValueMaps[i].add(k, mappedValue)
+
+    print("{}_source".format(i))
+    if len(indexValueMaps[i].valueMap) > 0:
+        redis.hmset("{}_source".format(i), indexValueMaps[i].valueMap)
+    print("{}_valueSet".format(i))
+    print(str(indexValueMaps[i].valueSet))
+    redis.sadd("{}_valueSet".format(i), str(indexValueMaps[i].valueSet))
+
+breakpoint()
 count = 0
 
 # Reference app: gitprojects/logbrowser
