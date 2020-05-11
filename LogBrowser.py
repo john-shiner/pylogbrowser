638c7c684dad28d9b25ed2b8ceca938857307f54
john-shiner <john.shiner@gmail.com>
2020-05-11 09:49:06 -0500

    fixed double content, cached analysis content

--- LogBrowser.py
+++ LogBrowser.py
@@ -25,14 +25,10 @@ class IndexMgr:
     def __init__(self, fieldName):
         self.fieldName = fieldName
         self.valueMap = {}
-        self.valueSet = set()
+        # self.valueSet = set()
 
         IndexMgr.indexValueManagers[fieldName] = self
-    def setadd(self, fieldValue):
-        ivm = IndexMgr.indexValueManagers
-        mgr = ivm[self.fieldName]
-        vs = mgr.valueSet
-        vs.add(str(fieldValue).strip())
+
     def maple(self, fieldValue, logEntryKey):
         ivm = IndexMgr.indexValueManagers
         mgr = ivm[self.fieldName]
@@ -49,14 +45,16 @@ class IndexMgr:
 
 class LogBrowser:
     _instance = None
+
     # import lb_command
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(cls, *args, **kwargs)
-            cls._instance.indexValueManagers = {}
+            # cls._instance.set_vm_dirtyFlag()
+            cls._instance.page_content = None
+            cls._instance.csv = None
             cls._instance.createAllIndexValueMaps()
-
         return cls._instance
 
     supportedIndices = ["client_host", "client_id", "client_ip", \
@@ -67,6 +65,16 @@ class LogBrowser:
                         "response_status_code", "soap_operation", \
                         "soap_siteId", "target_basepath", "target_host", \
                         "target_ip", "virtual_host"]
+
+    def vm_dirtyFlag(self):
+        return bool(redis.get("vm_dirtyFlag"))
+
+    def clear_vm_dirtyFlag(self):
+        redis.set("vm_dirtyFlag", "False")
+
+    def set_vm_dirtyFlag(self):
+        redis.set("vm_dirtyFlag", "True")
+        LogBrowser._instance.page_content = None
 
     # Store for index-value-logEntry maps as they are created
     def indexValueMaps():
@@ -86,23 +94,48 @@ class LogBrowser:
         return self._logEntries.keys()
 
     def analysis_page_content(self):
+        if LogBrowser._instance.page_content:
+            return LogBrowser._instance.page_content
+        else:
+            ivm = IndexMgr.indexValueManagers
 
-        ivm = IndexMgr.indexValueManagers
+            page_content = ""
+            for i in self.supportedIndices:
+                mgr = ivm[i]
+                vm = mgr.valueMap
+                # vs = mgr.valueSet
+                page_content +="<h4>{}</h4>".format(i)
+                page_content +="<ul>"
+                # breakpoint()
+                for j in vm.keys():
+                    page_content += "<li>Value '{}' mapped to {} logEntries</li>".format(j, len(vm[j]))
+                    page_content += "<p>---> mapped logEntries -----    "
+                    page_content += "map:{}:{}</p>".format(i, j)
+                page_content +="</ul>"
+            LogBrowser._instance.page_content = page_content
+            return page_content
 
-        page_content = ""
-        for i in self.supportedIndices:
-            mgr = ivm[i]
-            vm = mgr.valueMap
-            vs = mgr.valueSet
-            page_content +="<h4>{}</h4>".format(i)
-            page_content +="<ul>"
-            # breakpoint()
-            for j in vm.keys():
-                page_content += "<li>Value '{}' mapped to {} logEntries</li>".format(j, len(vm[j]))
-                page_content += "<p>---> mapped logEntries -----    "
-                page_content += "map:{}:{}</p>".format(i, j)
-            page_content +="</ul>"
-        return page_content
+    def analysis_csv(self):
+        if LogBrowser._instance.csv:
+            return LogBrowser._instance.csv
+        else:
+            ivm = IndexMgr.indexValueManagers
+            row_header = "{}|{}|{}\n".format("Index", "Value", "Count")
+            row_content = row_header
+
+            for i in self.supportedIndices:
+                mgr = ivm[i]
+                vm = mgr.valueMap
+                # vs = mgr.valueSet
+                first_field = "{}|".format(i)
+
+                # breakpoint()
+                for j in vm.keys():
+                    row_content += first_field
+                    row_content += "{}|".format(j)
+                    row_content += "{}\n".format(len(vm[j]))
+            LogBrowser._instance.csv = row_content
+            return row_content
 
     def createAllIndexValueMaps(self):
         """ Creates a value-to-LogEntry map for the specified indexName"""
@@ -110,37 +143,40 @@ class LogBrowser:
         for indexName in self.supportedIndices:
             indexMgr = IndexMgr(indexName)
             self.loadIndexValueMap(indexName)
+            self.clear_vm_dirtyFlag()
 
     def loadIndexValueMap(self, indexName):
         """ Creates a value-to-LogEntry map for the specified indexName"""
 
         indexMgr = IndexMgr.indexValueManagers[indexName]
-
-        # scan the logEntries that have been loaded 
-
-        for le in redis.scan_iter(match="logEntry:*", count='100'):
-            v = redis.hget(le, indexName)
-
-            fieldVal = v
-            if len(fieldVal) == 0:
-                fieldVal="None"
-
-            indexMgr.setadd(fieldVal)
-
-            indexMgr.maple(fieldVal, le)
-
         vm = indexMgr.valueMap
-        # breakpoint()
-        # pipe.set("idx:{}".format(indexName), vm)
-        # print("idx:{}".format(indexName))
 
-        for i in vm.keys():
-            storable = [ int(x.split(":")[1]) for x in vm[i]]
-            pipe.set("idx:{}:{}".format(indexName, i), "map:{}:{}".format(indexName, i) )
-            # print("idx:{}:{}".format(indexName, i))
-            pipe.set("map:{}:{}".format(indexName, i), str(sorted(storable)))
-            # print("map:{}:{}".format(indexName, i))
-        pipe.execute()
+        if not self.vm_dirtyFlag():
+            for i in redis.scan_iter(match="map:{}:*".format(indexName), count=100):
+                valueName = i.split(":")[2]
+                print(valueName)
+                breakpoint()
+                vm[valueName] = redis.get("map:{}:{}".format(indexName, valueName)).strip('][').split(', ')
+        else:
+
+            # scan the logEntries that have been loaded 
+
+            for le in redis.scan_iter(match="logEntry:*", count='100'):
+                v = redis.hget(le, indexName)
+
+                fieldVal = v
+                if len(fieldVal) == 0:
+                    fieldVal="None"
+
+                indexMgr.maple(fieldVal, le)
+
+            for i in vm.keys():
+                storable = [ int(x.split(":")[1]) for x in vm[i]]
+                pipe.set("idx:{}:{}".format(indexName, i), "map:{}:{}".format(indexName, i) )
+                # print("idx:{}:{}".format(indexName, i))
+                pipe.set("map:{}:{}".format(indexName, i), str(sorted(storable)))
+                # print("map:{}:{}".format(indexName, i))
+            pipe.execute()
 
     def getLoadedFiles(self):
         return sorted(redis.smembers("loadedLogFiles"))
@@ -148,6 +184,8 @@ class LogBrowser:
     def loadLogFile(self, filePath):
         ###################### filePath = directory + filename
         # Import Log files
+        self.set_vm_dirtyFlag()
+
         print("Importing ...")
         if filePath in self.getLoadedFiles():
             print("File {} is already loaded".format(filePath))
@@ -208,19 +246,6 @@ class LogBrowser:
                     # Populate the logEntryKey hash for each logEntry key-value pair
                     pipe.hset(logEntryKey, k, v )
 
-                    # # Populate the IndexMgr data
-                    # fieldVal = v
-
-                    # if len(fieldVal) == 0:
-                    #     fieldVal="None"
-
-                    # if indexName in self.supportedIndices:
-                    #     indexMgr = IndexMgr.indexValueManagers[indexName]
-
-                    #     indexMgr.setadd(fieldVal)
-
-                    #     indexMgr.maple(fieldVal, logEntryKey)
-                    # More messy fields
                     pipe.hset(logEntryKey, "request_content", "PLACEHOLDER")
                     pipe.hset(logEntryKey, "response_payload", "PLACEHOLDER")
                     pipe.hset(logEntryKey, "logFileName", sourceFileName)
@@ -231,10 +256,26 @@ class LogBrowser:
 
         print("Import of {} records completed.".format(le_count))
 
+# indexName="client_host"
+# lb = LogBrowser()
+# for i in redis.scan_iter(match="map:{}:*".format(indexName), count=100):
+#     valueName = i.split(":")[2]
+#     print(valueName)
+#     breakpoint()
+#     ivm = IndexMgr.indexValueManagers
+#     vm = ivm[indexName].valueMap
+#     vm[valueName] = redis.get("map:{}:{}".format(indexName, valueName)).strip('][').split(', ')
+#     vm[valueName]
+
 # os.system("redis-cli smembers loadedLogFiles")
 
-# lb = LogBrowser()
-# lb.createAllIndexValueMaps()
+# output = lb.analysis_csv()
+
+# f = open('analysis.csv', 'w')
+# f.write(output)
+# f.close()
+
+
 # ivm = IndexMgr.indexValueManagers
 # ivm['request_path'].valueMap
 # ivm['request_path'].valueSet
--- app.py
+++ app.py
@@ -197,4 +197,4 @@ def showLogEntry():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-    # app.run(debug=True,use_reloader=False)
+    # app.run(host="0.0.0.0", debug=True, use_reloader=False)
